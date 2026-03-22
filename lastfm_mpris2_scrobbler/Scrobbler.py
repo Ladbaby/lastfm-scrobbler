59e0026b608e8180046c89bac6373b2d9a13b2e8
Ladbaby <1390265970@qq.com>
2026-03-22 13:16:37 +0800

    refactor(Scrobbler):

--- lastfm_mpris2_scrobbler/Scrobbler.py
+++ lastfm_mpris2_scrobbler/Scrobbler.py
@@ -13,45 +13,23 @@ class Scrobbler:
         coloredlogs.install(level=kwargs["log_level"], logger=logger)
         self.player_dict: dict[str, PlayerState] = {} # key: player_uri
 
-        # TODO: handle network error
-        self.network = None
-        self.init_network()
+        self.network: pylast.LastFMNetwork | None = None
+        self._init_network()
             
         # supports wildcard, see matches_whitelist() below
-        self.application_whitelist = kwargs["application_whitelist"]
+        self.application_whitelist: list[str] = kwargs["application_whitelist"]
 
         # scrobbler will upload if the track has been played for 4 mins or half the total length
-        self.scrobble_time_threshold = int(kwargs["scrobble_time_threshold"])
+        self.scrobble_time_threshold: int = int(kwargs["scrobble_time_threshold"])
 
         # offline cache
         self.cache = Cache()
-
-    def init_network(self):
-        try:
-            logger.debug("Try logging in now...")
-            self.network = pylast.LastFMNetwork(
-                api_key=self.kwargs["api_key"],
-                api_secret=self.kwargs["api_secret"],
-                username=self.kwargs["user_name"],
-                password_hash=self.kwargs["password_hash"],
-            )
-            logger.info(f"Successfully login to Last.fm as {self.kwargs['user_name']}")
-        except Exception as e:
-            logger.debug(f"LastFMNetwork initialization failed: {e=}")
-            logger.warning("Running in OFFLINE mode")
-
-    def matches_whitelist(self, uri):
-        """Check if URI matches any pattern in the whitelist."""
-        for pattern in self.application_whitelist:
-            if fnmatch.fnmatch(uri, pattern):
-                return True
-        return False
 
     def connect_to_all_players(self):
         last_observation_uri_list = list(self.player_dict)
         for uri in get_players_uri():
             uri = str(uri)
-            if self.matches_whitelist(uri):
+            if self._matches_whitelist(uri):
                 p = Player(dbus_interface_info={'dbus_uri': uri})
                 if uri in self.player_dict.keys():
                     self.player_dict[uri].update_status(p.Metadata, p.PlaybackStatus, get_unix_timestamp())
@@ -63,14 +41,14 @@ class Scrobbler:
 
     def scrobble_all_players(self):
         if self.network is None:
-            self.init_network()
+            self._init_network()
         scrobble_list = []
         for uri, player_obj in self.player_dict.items():
             if player_obj.playback_status == "Playing" and self.network is not None:
                 try:
                     self.network.update_now_playing(
                         artist=player_obj.artist,
-                        title=self.fix_title(player_obj.title),
+                        title=self._fix_title(player_obj.title),
                         album=player_obj.album,
                         album_artist=player_obj.albumArtist,
                         track_number=player_obj.trackNumber,
@@ -84,30 +62,80 @@ class Scrobbler:
                 logger.debug("scrobble condition unmet")
 
         if self.network is not None:
-            self.scrobble_list(scrobble_list, isCache=False)
+            self._scrobble_list(scrobble_list, isCache=False)
             # [(trackid, artist, title, timestamp, album, album_artist, track_number, duration), ...]
             cache_list = self.cache.read_unscrobbled()
             if cache_list is not None:
                 cache_list_converted = []
                 for cache in cache_list:
                     cache_list_converted.append(PlayerState().set_value(
-                        trackid=cache[0],
-                        artist=cache[1],
-                        title=cache[2],
-                        timestamp=cache[3],
                         album=cache[4],
                         album_artist=cache[5],
+                        artist=cache[1],
+                        duration=cache[7],
+                        timestamp=cache[3],
+                        title=cache[2],
                         track_number=cache[6],
-                        duration=cache[7]
+                        trackid=cache[0]
                     ))
-                self.scrobble_list(cache_list_converted, isCache=True)
+                self._scrobble_list(cache_list_converted, isCache=True)
         elif len(scrobble_list) > 0:
             self.cache.write_unscrobbled(scrobble_list)
             logger.debug("Fail to scrobble now. Write to cache")
+    
+    def _fix_title(self, title):
+        audio_formats = [
+            '.mp3', 
+            '.ogg', 
+            '.wav', 
+            '.aac', 
+            '.flac'
+        ]
+        for format in audio_formats:
+            if title.endswith(format):
+                return title[:-(len(format))]
+        return title
 
-    def scrobble_list(self, obj_list, isCache = False):
+    def _init_network(self):
+        try:
+            logger.debug("Try logging in now...")
+            self.network = pylast.LastFMNetwork(
+                api_key=self.kwargs["api_key"],
+                api_secret=self.kwargs["api_secret"],
+                username=self.kwargs["user_name"],
+                password_hash=self.kwargs["password_hash"],
+            )
+            logger.info(f"Successfully login to Last.fm as {self.kwargs['user_name']}")
+        except Exception as e:
+            logger.debug(f"LastFMNetwork initialization failed: {e=}")
+            logger.warning("Running in OFFLINE mode")
+
+    def _matches_whitelist(self, uri):
+        """Check if URI matches any pattern in the whitelist."""
+        for pattern in self.application_whitelist:
+            if fnmatch.fnmatch(uri, pattern):
+                return True
+        return False
+
+    def _obj_list_to_dict_list(self, obj_list: list[PlayerState]):
+        dict_list = []
+        for obj in obj_list:
+            dict_list.append(
+                {
+                    "album": obj.album,
+                    "album_artist": obj.albumArtist,
+                    "artist": obj.artist,
+                    "duration": obj.length,
+                    "timestamp": obj.last_observation_timestamp,
+                    "title": self._fix_title(obj.title),
+                    "track_number": obj.trackNumber
+                }
+            )
+        return dict_list
+    
+    def _scrobble_list(self, obj_list, isCache = False):
         if obj_list != None and len(obj_list) > 0:
-            dict_list = self.obj_list_to_dict_list(obj_list)
+            dict_list = self._obj_list_to_dict_list(obj_list)
             try:
                 self.network.scrobble_many(dict_list)
                 logger.info("scrobbled: " + str(dict_list))
@@ -120,32 +148,3 @@ class Scrobbler:
                 else:
                     logger.debug("Fail to scrobble cached record now")
                     pass
-
-    def obj_list_to_dict_list(self, obj_list):
-        dict_list = []
-        for obj in obj_list:
-            dict_list.append(
-                {
-                    "artist": obj.artist,
-                    "title": self.fix_title(obj.title),
-                    "timestamp": obj.last_observation_timestamp,
-                    "album": obj.album,
-                    "album_artist": obj.albumArtist,
-                    "track_number": obj.trackNumber,
-                    "duration": obj.length,
-                }
-            )
-        return dict_list
-    
-    def fix_title(self, title):
-        audio_formats = [
-            '.mp3', 
-            '.ogg', 
-            '.wav', 
-            '.aac', 
-            '.flac'
-        ]
-        for format in audio_formats:
-            if title.endswith(format):
-                return title[:-(len(format))]
-        return title
